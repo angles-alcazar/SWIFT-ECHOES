7228f07896954bb57b6214786b979b08054a489d
Sagan Sutherland <sagan.sutherland@uconn.edu>
2025-12-23 18:32:27 -0500

    add build_echoes.sh for easier compile commands

---
+++ build_echoes.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+function usage() {
+    cat <<EOF
+Usage:
+  -d, --debug   build SWIFT with debug information
+  -o, --opt     build SWIFT with optimizations on
+  -h, --help    show this message
+EOF
+}
+
+PARMETIS=/mnt/home/ssutherland/codes/libs/parmetis/
+ONETBB=/mnt/home/ssutherland/codes/libs/oneTBB/
+FLAGS="--enable-fof --with-black-holes=ECHOES --enable-optimizations=no --with-hydro=sphenix --with-parmetis=${PARMETIS} --with-tbbmalloc=${ONETBB}"
+
+for a in "$@"; do
+    case $a in
+        -d|--debug)
+            FLAGS="$FLAGS --enable-debug=yes --enable-debugging-checks"
+            ;;
+        -o|--opt)
+            FLAGS="$FLAGS --enable-optimizations=yes --with-gcc-arch=native"
+            ;;
+        -h|--help)
+            usage
+            exit 2
+            ;;
+        *)
+            ;;
+    esac
+done
+
+module load modules/2.3-20240529
+module load openblas/single-0.3.26
+module load gcc/11.4.0
+module load openmpi/4.0.7
+module load hdf5/1.12.3
+module load gsl/2.7.1
+module load fftw/mpi-3.3.10
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/mnt/home/ssutherland/codes/libs/parmetis/"
+
+./autogen.sh
+./configure $FLAGS
+make clean
+make "-j$(nproc)"
