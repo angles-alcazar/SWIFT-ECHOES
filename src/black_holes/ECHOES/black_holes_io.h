b1cd7775b28af0deaee336bb43b765cd56b1af06
Sagan Sutherland <sagan@farthinghalearms.com>
2026-04-03 13:34:33 -0400

    Central galaxy flag is a physical field

--- src/black_holes/ECHOES/black_holes_io.h
+++ src/black_holes/ECHOES/black_holes_io.h
@@ -156,10 +156,11 @@ INLINE static void black_holes_write_particles(const struct bpart *bparts,
       "MaxGroupMasses", FLOAT, 1, UNIT_CONV_MASS, 0.f, bparts,
       fof_galaxy_data.max_group_mass, "Maximum reached group mass.");
 
-  list[num++] = io_make_output_field("Central", INT, 1, UNIT_CONV_NO_UNITS, 0.f,
-                                     bparts, fof_galaxy_data.is_central,
-                                     "Is the particle considering itself the "
-                                     "central galaxy in its host halo?");
+  list[num++] =
+      io_make_physical_output_field("Central", INT, 1, UNIT_CONV_NO_UNITS, 0.f,
+                                    bparts, fof_galaxy_data.is_central, 0,
+                                    "Is the particle considering itself the "
+                                    "central galaxy in its host halo?");
 
   list[num++] = io_make_physical_output_field(
       "ParticleIDs", ULONGLONG, 1, UNIT_CONV_NO_UNITS, 0.f, bparts, id,
